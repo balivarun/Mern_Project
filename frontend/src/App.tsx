4b796a9c574646ce12297b08a7d460e39bead26b
Varun Repo <varunbali345@gmail.com>
2025-04-02 06:14:17 +0530

    install bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
-
+import bootstrap from '/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Navbar from './components/Navbar'
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className='App'>
+      <Navbar/>
+    </div>
   )
 }
 
