e6fc06d3b16c3806624652e87c3368a0fa71802d
balivarun <varunbali345@gamil.com>
2025-04-12 15:16:18 +0530

    added navabar to the website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
-import bootstrap from '/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 function App() {
 
   return (
